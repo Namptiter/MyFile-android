8e470c1a8c39ac2db30410fdb8fd59520654670d
Nam <44342421+Namptiter@users.noreply.github.com>
2021-07-10 17:46:52 +0700

    Template Design
    Render list view

--- app/src/main/java/com/example/myfile/MainActivity.java
+++ app/src/main/java/com/example/myfile/MainActivity.java
@@ -13,75 +13,79 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<mFolder> folderList;
-    MyAdapterFolder folferAdapter;
-    ListView view_List_Folder;
+    ArrayList<mFile> fileList;
+    MyAdapterFile fileAdapter;
+    ListView view_List_File;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        folderList.add(new mFolder(0,"file.txt","60Kb"));
-        folderList.add(new mFolder(1,"file2.txt","100Mb"));
 
-        folferAdapter = new MyAdapterFolder(folderList);
-        view_List_Folder = findViewById(R.id.list_folder);
-        view_List_Folder.setAdapter(folferAdapter);
+        fileList = new ArrayList<>();
+        fileList.add(new mFile(0,"Folder1","","folder"));
+        fileList.add(new mFile(1,"Folder2","","folder"));
+        fileList.add(new mFile(2,"Folder3","","folder"));
+        fileList.add(new mFile(3,"file1.txt","12kb","file"));
+        fileList.add(new mFile(4,"file2.txt","5kb","file"));
+        fileList.add(new mFile(5,"file3.txt","6kb","file"));
+        fileList.add(new mFile(6,"Folder3","","folder"));
+        fileList.add(new mFile(7,"Folder4","","folder"));
+        fileList.add(new mFile(8,"Folder5","","folder"));
+        fileAdapter = new MyAdapterFile(fileList);
+        view_List_File = findViewById(R.id.list_file);
+        view_List_File.setAdapter(fileAdapter);
     }
 
     class mFile{
         int id;
         String name;
         String size;
-        public mFile(int id, String name, String size){
+        String type;
+        public mFile(int id, String name, String size, String type){
             this.id = id;
             this.name = name;
             this.size = size;
+            this.type = type;
         }
     }
-    class mFolder{
-        int id;
-        String name;
-        String size;
-        public mFolder(int id, String name, String size){
-            this.id = id;
-            this.name = name;
-            this.size = size;
+    class MyAdapterFile extends BaseAdapter{
+        final ArrayList<mFile> fileList;
+        MyAdapterFile(ArrayList<mFile> fileList){
+            this.fileList = fileList;
         }
-    }
-
-    class MyAdapterFolder extends BaseAdapter{
-        final ArrayList<mFolder> folderList;
-        MyAdapterFolder(ArrayList<mFolder> folderList) {
-            this.folderList = folderList;
-        }
-
         @Override
         public int getCount() {
-            return folderList.size();
+            return fileList.size();
         }
 
         @Override
         public Object getItem(int i) {
-            return folderList.get(i);
+            return fileList.get(i);
         }
 
         @Override
         public long getItemId(int i) {
-            return folderList.get(i).id;
+            return fileList.get(i).id;
         }
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
+            mFile f = (mFile) getItem(i);
             View myView;
-            if(view==null){
-                myView = View.inflate(viewGroup.getContext(),R.layout.folder_view,null);
-            }else myView = view;
-
-            mFolder f = (mFolder) getItem(i);
-            ((TextView) myView.findViewById(R.id.folder_name)).setText(String.format("%s"), TextView.BufferType.valueOf(f.name));
-            ((TextView) myView.findViewById(R.id.folder_size)).setText(String.format("%s"), TextView.BufferType.valueOf(f.size));
-
-            return myView;
+            if(f.type=="folder"){
+                if(view==null){
+                    myView = View.inflate(viewGroup.getContext(),R.layout.folder_view,null);
+                }else myView = view;
+                ((TextView) myView.findViewById(R.id.folder_name)).setText(String.format("%s",f.name));
+                return myView;
+            }else {
+                if(view==null){
+                    myView = View.inflate(viewGroup.getContext(),R.layout.file_view,null);
+                }else myView = view;
+                ((TextView) myView.findViewById(R.id.file_name)).setText(String.format("%s",f.name));
+                ((TextView) myView.findViewById(R.id.file_size)).setText(String.format("%s",f.size));
+                return myView;
+            }
         }
     }
 }
